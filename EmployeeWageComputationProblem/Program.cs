dfdbad656e5c0f834506161107d4381c8f4373e9
Karan kanchan <kanchan.karan.17ce7002@gmail.com>
2021-08-07 11:46:15 +0530

    Added Employee Presenty Functionality

--- EmployeeWageComputationProblem/Program.cs
+++ EmployeeWageComputationProblem/Program.cs
@@ -6,7 +6,19 @@ namespace EmployeeWageComputationProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("WELCOME TO EMPLOYEE WAGE COMPTATION");
+            int full_day = 1;
+            Random random = new Random();
+            int day = random.Next(0, 2);
+
+            if(day==full_day)
+            {
+                Console.WriteLine("Employee is Present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
