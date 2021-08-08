a2f99c9b60285d0236f743e7823bb7385b5de677
Karan kanchan <kanchan.karan.17ce7002@gmail.com>
2021-08-08 23:07:36 +0530

    Added EmployeeDescription Functionality

---
+++ EmployeeWageComputationProblem/EmployeeWageDescription.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageComputationProblem
+{
+    class EmployeeWageDescription
+    {
+
+
+        public void Presenty()
+        {
+            int full_day = 1;
+            Random random = new Random();
+            int day = random.Next(0, 2);
+
+            if (day == full_day)
+            {
+                Console.WriteLine("Employee is Present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+
+        public void Salary()
+        {
+            int Wage_hourly = 20;
+            Console.WriteLine("Enter the number of working hours");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            int wage = hours * Wage_hourly;
+
+            Console.WriteLine($"Employee wage is {wage}");
+        }
+
+        /* 
+         * public void Salary()
+        {
+            int Wage_hourly = 20;
+            Console.WriteLine("Enter the number of working hours");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            int wage = hours * Wage_hourly;
+
+            Console.WriteLine($"Employee wage is {wage}");
+        }*/
+
+        public void SalaryGross()
+        {
+            int n;
+            float basic_sal, dearness_all, comm, house_rent_all, govt_tax, gross_sal, total_salary = 0;
+            for (n = 1; n <= 3; n++)
+            {
+                Console.Write("Enter Basic Salary for {0} staff : ", n);
+                basic_sal = float.Parse(Console.ReadLine());
+                dearness_all = basic_sal * 20 / 100;
+                comm = basic_sal * 10 / 100;
+                house_rent_all = basic_sal * 25 / 100;
+                govt_tax = basic_sal * 20 / 100;
+                gross_sal = basic_sal + dearness_all + comm + house_rent_all - govt_tax;
+                total_salary = total_salary + gross_sal;
+                Console.Write("Basic Salary of Employee = {0} \n", basic_sal);
+                Console.Write("Dearness Allowance = {0} \n", dearness_all);
+                Console.Write("Commission = {0} \n", comm);
+                Console.Write("House Rent Allowance = {0} \n", house_rent_all);
+                Console.Write("Government Tax = {0} \n", govt_tax);
+                Console.Write("Gross Salary = {0} \n\n", gross_sal);
+            }
+            Console.Write("\n \n Total Salary Payable to All Staff = {0}", total_salary);
+
+        }
+    }
+}
--- EmployeeWageComputationProblem/Program.cs
+++ EmployeeWageComputationProblem/Program.cs
@@ -5,22 +5,63 @@ namespace EmployeeWageComputationProblem
     class Program
     {
         public int Wage_Per_Hour = 20;
+        int n1;
+        float basic_sal1, dearness_all1, comm1, house_rent_all1, govt_tax1, gross_sal1, total_salary1 = 0;
         static void Main(string[] args)
         {
-            int Wage_hourly = 20;
+            int Wage_hourly = 30;
             Random random = new Random();
             int day = random.Next(0, 2);
 
-            Console.WriteLine("WELCOME TO EMPLOYEE WAGE COMPUTATION\n");
+            bool loopContinue = true;
+            while (loopContinue)
+            {
+                Console.WriteLine("1.Program to Check Attendance ");
+                Console.WriteLine("2.Program to Get Salary ");
+                Console.WriteLine("3.Program to Get Monthly Gross Salary");
+                Console.WriteLine("Please select a program");
+                int input = Convert.ToInt32(Console.ReadLine());
 
-            EmployeeDetails employeedetails = new EmployeeDetails();
-            employeedetails.Presenty();
-            EmployeeSalary salary = new EmployeeSalary();
-            salary.Salary();
+                switch (input)
+                {
+                    case 1:
+                        Console.WriteLine("WELCOME TO EMPLOYEE DETAILSS PROGRAM TO CHECK ATTENDANCE\n");
+                        EmployeeWageDescription employeeS1 = new EmployeeWageDescription();
+                        employeeS1.Presenty();
+                        break;
+                    case 2:
+                        Console.WriteLine("WELCOME TO EMPLOYEE DETAILS PROGRAM TO CALCULATE  THE EMPLOYEE WAGE \n");
+                        EmployeeWageDescription employeeS2 = new EmployeeWageDescription();
+                        employeeS2.Salary();
+                        break;
+                    case 3:
+                        Console.WriteLine("WELCOME TO EMPLOYEE DETAILS PROGRAM\n");
+                        EmployeeWageDescription employeeS3 = new EmployeeWageDescription();
+                        employeeS3.SalaryGross();
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Option");
+                        break;
+                }
+                if (loopContinue)
+                    Console.WriteLine("Please enter a valid choice.");
 
 
-         }
-        
+
+
+
+            }
+
+            /*Console.WriteLine("WELCOME TO EMPLOYEE DETAILS WE WILLE THE EMPLOYEE WAGE AND OTHER DETAILS\n");
+        EmployeeWageDescription employeeS= new EmployeeWageDescription();
+        employeeS.Presenty();
+        employeeS.Salary();
+        employeeS.SalaryGross();*/
+
+
+        }
+
     }
 
 }
